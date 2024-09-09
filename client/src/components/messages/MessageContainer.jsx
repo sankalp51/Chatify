b77d8924677f7d6f9c932fdce5973a095394c0d8
sankalp kalangutkar <sankalpkalangutkar51@gmail.com>
2024-09-09 17:24:55 +0530

    chat view bug fix

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,26 +1,32 @@
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import NotChatSelected from "../NoChatSelected";
+import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
 
 export default function MessageContainer() {
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+    useEffect(() => {
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation]);
     return (
-        <>{!noChatSelected ? (
-            <section className="flex flex-col flex-grow h-full">
-                <header className="bg-slate-500 px-4 py-2">
-                    <span className="label-text">To:</span>
-                    <span className="text-gray-900 font-bold">John Doe</span>
-                </header>
-                <div className="flex-1 overflow-auto px-4">
-                    <Messages />
-                </div>
-                <div className="p-4">
-                    <MessageInput />
-                </div>
-            </section>
-        ) : (
-            <NotChatSelected />
-        )}
+        <>
+            {selectedConversation ? (  // Check if selectedConversation exists (truthy)
+                <section className="flex flex-col flex-grow h-full">
+                    <header className="bg-slate-500 px-4 py-2">
+                        <span className="label-text">To:</span>
+                        <span className="text-gray-900 font-bold">{selectedConversation?.fullName}</span>
+                    </header>
+                    <div className="flex-1 overflow-auto px-4">
+                        <Messages />
+                    </div>
+                    <div className="p-4">
+                        <MessageInput />
+                    </div>
+                </section>
+            ) : (  // Fallback UI when no conversation is selected
+                <NotChatSelected />
+            )}
         </>
     );
 }
