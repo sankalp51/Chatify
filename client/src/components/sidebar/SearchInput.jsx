2f05e302a78b7c96d82175605d37ae326c559a0e
sankalp kalangutkar <sankalpkalangutkar51@gmail.com>
2024-09-10 22:36:06 +0530

    implemented chat search feature

--- client/src/components/sidebar/SearchInput.jsx
+++ client/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,38 @@
 import Input from "../Input";
 import Button from '../Button';
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import useConversation from "../../zustand/useConversation";
+import useGetConversation from "../../hooks/useGetConversations"
+import { toast } from "sonner";
 
 export default function SearchInput() {
+    const [searchItem, setSearchItem] = useState("");
+    const { setSelectedConversation } = useConversation();
+    const { conversations } = useGetConversation();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!searchItem) return;
+        if (searchItem.length < 3) {
+            return toast.error("search must be more than 3 characters long")
+        }
+
+        const conversation = conversations.find(c => c.fullName.toLowerCase().includes(searchItem.toLowerCase()))
+        if (conversation) {
+            setSelectedConversation(conversation);
+            setSearchItem("");
+        }
+        else {
+            toast.error("No such user found");
+        }
+    }
+
     return (
-        <form className="flex items-center gap-2 w-full">
+        <form className="flex items-center gap-2 w-full" onSubmit={handleSubmit}>
             <div className="flex-1">
                 <Input
+                    value={searchItem}
+                    onChange={e => setSearchItem(e.target.value)}
                     type="text"
                     placeholder="Search..."
                     className="input input-bordered rounded-full w-full"
