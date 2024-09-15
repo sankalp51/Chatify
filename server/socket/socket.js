68bb9b40779bc5059921f452a288a592054fdb9a
sankalp51 <sankalpkalangutkar51@gmail.com>
2024-09-15 13:58:08 +0530

    prepared for production

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -4,12 +4,7 @@ const express = require("express");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: [`${process.env.CLIENT_ENDPOINT}`],
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
 
 const userSocketMap = {};
 
