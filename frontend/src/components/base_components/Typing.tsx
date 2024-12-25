aa6da0d139bafe4c9f46c16c03c2516a1b18a94c
sankalp51 <sankalpkalangutkar51@gmail.com>
2024-12-26 00:09:31 +0530

    some changes

--- frontend/src/components/base_components/Typing.tsx
+++ frontend/src/components/base_components/Typing.tsx
@@ -3,37 +3,40 @@ import { useEffect, useRef } from "react";
 
 export default function Typing() {
   const bubbleRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     bubbleRef.current?.scrollIntoView();
   }, []);
+
   return (
     <div
-      className="rounded-[20px] p-[10px] max-w-[10%] bg-secondary m-2"
+      // Adjust the classes below to fit your design / theme
+      className="m-2 rounded-[20px] py-[15px] px-[15px] bg-secondary w-[15%] md:w-[11%]"
       ref={bubbleRef}
     >
       {/* Typing dots */}
       <div className="flex items-center space-x-2">
         <motion.div
-          className="w-3 h-3 bg-gray-500 rounded-full"
-          animate={{ opacity: [0.3, 1, 0.3] }}
-          transition={{ repeat: Infinity, duration: 1, ease: "easeInOut" }}
+          className="w-2 h-2 bg-gray-500 rounded-full"
+          animate={{ y: [0, -10, 0] }}
+          transition={{ repeat: Infinity, duration: 0.6, ease: "easeInOut" }}
         />
         <motion.div
-          className="w-3 h-3 bg-gray-500 rounded-full"
-          animate={{ opacity: [0.3, 1, 0.3] }}
+          className="w-2 h-2 bg-gray-500 rounded-full"
+          animate={{ y: [0, -10, 0] }}
           transition={{
             repeat: Infinity,
-            duration: 1,
+            duration: 0.6,
             ease: "easeInOut",
             delay: 0.2,
           }}
         />
         <motion.div
-          className="w-3 h-3 bg-gray-500 rounded-full"
-          animate={{ opacity: [0.3, 1, 0.3] }}
+          className="w-2 h-2 bg-gray-500 rounded-full"
+          animate={{ y: [0, -10, 0] }}
           transition={{
             repeat: Infinity,
-            duration: 1,
+            duration: 0.6,
             ease: "easeInOut",
             delay: 0.4,
           }}
